5a3eef4c9e36140f418252c984349eea52d0c045
lxfly2000 <gaoboyuan8@qq.com>
2016-05-01 19:03:43 +0800

    Considered parameters given by windows.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,15 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPvI, LPTSTR lpCmdLine, int 
 {
 	SetThreadUILanguage(GetUserDefaultUILanguage());
 	GeometryScreenSaver saver;
-	if (lstrcmp(lpCmdLine, TEXT("/S")) == 0 || lstrcmp(lpCmdLine, TEXT("/s")) == 0)
-		return saver.Run();
-	else return saver.Settings(hInstance);
+	if (lstrlen(lpCmdLine) == 0)
+		saver.Settings(hInstance);
+	else switch (lpCmdLine[1])
+	{
+	case TEXT('c'):case TEXT('C'):saver.Settings(hInstance); break;
+	case TEXT('t'):case TEXT('T'):saver.Run(); break;
+	case TEXT('p'):case TEXT('P'):break;
+	case TEXT('a'):case TEXT('A'):break;
+	case TEXT('s'):case TEXT('S'):saver.Run(); break;
+	}
+	return 0;
 }
